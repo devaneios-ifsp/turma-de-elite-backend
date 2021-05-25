f8b569460e78eb5404571fc6a1afe69719dd3b7d
André Gomes <andre.montero702@gmail.com>
2021-05-25 11:42:11 -0300

    Criado projeto utilizando Spring Initializr

---
+++ src/main/java/com/devaneios/turmadeelite/TurmaDeEliteApplication.java
@@ -0,0 +1,13 @@
+package com.devaneios.turmadeelite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurmaDeEliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurmaDeEliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devaneios/turmadeelite/TurmaDeEliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devaneios.turmadeelite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurmaDeEliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
