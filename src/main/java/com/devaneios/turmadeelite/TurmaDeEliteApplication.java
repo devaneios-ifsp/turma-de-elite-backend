23fa49384ec0396f96b5521e48a2855b7ed27850
André Gomes <andre.montero702@gmail.com>
2021-06-11 10:18:06 -0300

    Pegando variáveis de ambiente do Firebase diretamente

--- src/main/java/com/devaneios/turmadeelite/TurmaDeEliteApplication.java
+++ src/main/java/com/devaneios/turmadeelite/TurmaDeEliteApplication.java
@@ -2,6 +2,8 @@ package com.devaneios.turmadeelite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
 public class TurmaDeEliteApplication {
--- src/main/java/com/devaneios/turmadeelite/configuration/firebase/FirebaseConfiguration.java
+++ src/main/java/com/devaneios/turmadeelite/configuration/firebase/FirebaseConfiguration.java
@@ -4,60 +4,35 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 
 import java.io.*;
+import java.util.Map;
 
 @Slf4j
 @Service
 public class FirebaseConfiguration {
 
-    @Value("${firebase.credentials.type}")
-    private String type;
-
-    @Value("${firebase.credentials.project-id}")
-    private String projectId;
-
-    @Value("${firebase.credentials.private-key-id}")
-    private String privateKeyId;
-
-    @Value("${firebase.credentials.private-key}")
-    private String privateKey;
-
-    @Value("${firebase.credentials.client-email}")
-    private String clientEmail;
-
-    @Value("${firebase.credentials.client-id}")
-    private String clientId;
-
-    @Value("${firebase.credentials.auth-uri}")
-    private String authUri;
-
-    @Value("${firebase.credentials.token-uri}")
-    private String tokenUri;
-
-    @Value("${firebase.credentials.auth-provider-cert-url}")
-    private String authProviderCertUrl;
-
-    @Value("${firebase.credentials.client-cert-url}")
-    private String clientCertUrl;
-
     public String firebaseAdminJsonFromEnv(){
+        Map<String, String> env = System.getenv();
         StringBuffer buffer = new StringBuffer();
         buffer.append("{");
-        buffer.append("\"").append("type").append("\":\"").append(type).append("\",");
-        buffer.append("\"").append("project_id").append("\":\"").append(projectId).append("\",");
-        buffer.append("\"").append("private_key_id").append("\":\"").append(privateKeyId).append("\",");
-        buffer.append("\"").append("private_key").append("\":\"").append(privateKey).append("\",");
-        buffer.append("\"").append("client_email").append("\":\"").append(clientEmail).append("\",");
-        buffer.append("\"").append("client_id").append("\":\"").append(clientId).append("\",");
-        buffer.append("\"").append("auth_uri").append("\":\"").append(authUri).append("\",");
-        buffer.append("\"").append("token_uri").append("\":\"").append(tokenUri).append("\",");
-        buffer.append("\"").append("auth_provider_x509_cert_url").append("\":\"").append(authProviderCertUrl).append("\",");
-        buffer.append("\"").append("client_x509_cert_url").append("\":\"").append(clientCertUrl).append("\"");
+        buffer.append("\"").append("type").append("\":\"").append(env.get("f_type")).append("\",");
+        buffer.append("\"").append("project_id").append("\":\"").append(env.get("f_project_id")).append("\",");
+        buffer.append("\"").append("private_key_id").append("\":\"").append(env.get("f_private_key_id")).append("\",");
+        buffer.append("\"").append("private_key").append("\":\"").append(env.get("f_private_key")).append("\",");
+        buffer.append("\"").append("client_email").append("\":\"").append(env.get("f_client_email")).append("\",");
+        buffer.append("\"").append("client_id").append("\":\"").append(env.get("f_client_id")).append("\",");
+        buffer.append("\"").append("auth_uri").append("\":\"").append(env.get("f_auth_uri")).append("\",");
+        buffer.append("\"").append("token_uri").append("\":\"").append(env.get("f_token_uri")).append("\",");
+        buffer.append("\"").append("auth_provider_x509_cert_url").append("\":\"").append(env.get("f_auth_provider_x509_cert_url")).append("\",");
+        buffer.append("\"").append("client_x509_cert_url").append("\":\"").append(env.get("f_client_x509_cert_url")).append("\"");
         buffer.append("}");
         return buffer.toString();
     }
@@ -67,7 +42,6 @@ public class FirebaseConfiguration {
     public FirebaseConfiguration() throws IOException {
         try{
             File file = ResourceUtils.getFile("firebase/firebase-sdk-key.json");
-
             FileInputStream serviceAccount = new FileInputStream(file);
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
