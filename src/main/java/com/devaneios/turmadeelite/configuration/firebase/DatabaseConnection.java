3972a98f26ad37df2163012005d68739400fc571
André Gomes <andre.montero702@gmail.com>
2021-07-14 07:11:10 -0300

    Criando DataSource manualmente quando em perfil de produção

---
+++ src/main/java/com/devaneios/turmadeelite/configuration/firebase/DatabaseConnection.java
@@ -0,0 +1,41 @@
+package com.devaneios.turmadeelite.configuration.firebase;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+@Slf4j
+public class DatabaseConnection {
+
+    @Bean
+    @Profile("production")
+    public DataSource postgresDataSource() {
+        String databaseUrl = System.getenv("HEROKU_POSTGRESQL_ONYX_URL");
+        log.info("Initializing PostgreSQL database: {}", databaseUrl);
+
+        URI dbUri;
+        try {
+            dbUri = new URI(databaseUrl);
+        }
+        catch (URISyntaxException e) {
+            log.error(String.format("Invalid DATABASE_URL: %s", databaseUrl), e);
+            return null;
+        }
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        DataSourceBuilder<?> builder = DataSourceBuilder.create();
+        builder.url(dbUrl);
+        builder.username(username);
+        builder.password(password);
+        return builder.build();
+    }
+}
