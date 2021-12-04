3334e607bf87c560f48ad6922247883ae5eb498c
NatanLisboa <natanflisboa1@gmail.com>
2021-12-04 11:15:14 -0300

    Removendo imports desnecessários

--- src/main/java/com/devaneios/turmadeelite/external/courses/ExternalCoursesController.java
+++ src/main/java/com/devaneios/turmadeelite/external/courses/ExternalCoursesController.java
@@ -7,10 +7,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
